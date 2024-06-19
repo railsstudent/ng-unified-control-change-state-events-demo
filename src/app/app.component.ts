e0bcf6522ac53722709fdb0f91dcfd4af4e01f0b
Connie Leung <cleung334@gmail.com>
2024-06-19 23:30:40 +0800

    chore:enable zoneless

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [],
+  template: '',
+  styles: '',
 })
 export class AppComponent {
   title = 'ng-unified-control-change-state-events-demo';
